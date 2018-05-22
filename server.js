f7899e1fe8900adec4229ab3266d87ace2844576
Sara <bracewell.sara@gmail.com>
2018-05-22 15:47:18 -0500

    updated styling added new pages, still have work

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
-const express = require('express');
+ const express = require('express');
 const path = require('path');
-
+let app = require('express')();
 const bodyParser = require('body-parser');
 
+//app.use(require('body-parser').urlencoded());
 
+// const CONTACT_ADDRESS = 'me@company.com';
+
+// let mailer = require('nodemailer').createTransport({
+//   service: 'Gmail',
+//   auth: {
+//     user: process.env.GMAIL_ADDRESS,
+//     pass: process.env.GMAIL_PASSWORD,
+//   }
+// });
+
+// app.post('/contact', function (req, res) {
+//   mailer.sendMail({
+//     from: req.body.from,
+//     to: [CONTACT_ADDRESS],
+//     subject: req.body.subject || '[No subject]',
+//     html: req.body.message || '[No message]',
+//   }, function (err, info) {
+//     if (err) return res.status(500).send(err);
+//     res.json({ success: true });
+//   })
+// });
 
 // Server will use port 3001.
 const PORT = process.env.PORT || 3001;
 // Yes, the app uses express.
-const app = express();
-
-
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
